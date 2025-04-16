603fa2a725819a23bf683ac1418177881d479df9
KUMAR21092001 <basakumar333@gmail.com>
2025-04-16 10:59:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Test Application - Kumar"
+    return "Welcome to Python Test Application - Kumar.com"
 
 
 if __name__ == "__main__":
